0e7f5bed8f1542d0c1d000bdca31618df57a6100
bennyhodl <ben@bitcoinbay.live>
2023-10-11 22:26:04 -0400

    bartender drink menu

---
+++ bartender.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+# Modified from: https://github.com/puzzle/lightning-beer-tap/blob/master/application.sh
+
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+usage(){
+cat << EOF                                                                              
+Usage: $0 [OPTIONS]                                          
+
+This script handles all interactions with the Bitcoin Bay Bartender.
+
+OPTIONS:
+	start	Initializes the device, starts the dashboard and bartender
+	stop	Stops all services
+	build	Build or rebuild the web socket to the lightning node 🦀
+                                                                                           
+EXAMPLES:                                                                               
+    application restart
+
+EOF
+}
+
+# Start all services on the beer tap device
+app_start(){
+  echo 🎵 hey, bartender 🎵
+
+	BARTENDER=${DIR}/target/debug/bartender
+	# Check if the websocket bridge has been built
+	if [ ! -f ${BARTENDER} ]; then
+		app_build
+	fi
+	# Start up the dashboard
+	source ${DIR}/dashboard.sh
+	# Hide mouse when still
+	#DISPLAY=:0 unclutter -idle 0.01 -root &
+	# Start websocket server, fork to background and pipe output to file
+  # RUST_LOG=info ${BARTENDER} & > ${DIR}/info.log
+  export RUST_LOG=info
+  nohup ${BARTENDER} > log.txt 2>&1 &
+}
+
+# Stop all services
+app_stop(){
+	echo "Killing all services..."
+	killall bartender
+	# pkill -o chromium
+	# killall unclutter
+}
+
+# Build or rebuild the java lighning node web bridge
+app_build(){
+	echo "Building the bartender web socket server 🦀"
+	cd ${DIR} && cargo build # >/dev/null 2>&1
+}
+
+# Argument parsing
+case $1 in
+	start)
+	app_start
+	exit 0;
+	;;
+
+	stop)
+	app_stop
+	exit 0;
+	;;
+
+	build)
+	app_build
+	exit 0;
+	;;
+
+	restart)
+	app_stop
+    app_start
+	exit 0;
+	;;
+
+	*)
+	usage
+	exit 0;
+	;;
+esac
---
+++ dashboard.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Modified from: https://github.com/puzzle/lightning-beer-tap/blob/master/dashboard/dashboard.sh
+
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+CHROME_ZOOM="1.0"
+
+KIOSK_ARGS="--kiosk --disable-translate --incognito --app=${DIR}/index.html --force-device-scale-factor=${CHROME_ZOOM}"
+
+# Don't sleep, don't blank, waste energy!
+
+os_name=$(uname -s)
+
+# Check if the system is Mac (Darwin) and execute the command accordingly
+if [ "$os_name" = "Darwin" ]; then
+    echo "Running on macOS"
+    open -a 'Brave Browser' ${DIR}/index.html
+elif [ "$os_name" = "Linux" ]; then
+    DISPLAY=:0 xset s off
+    DISPLAY=:0 xset s noblank
+    DISPLAY=:0 xset -dpms
+
+    DISPLAY=:0 nohup chromium-browser $KIOSK_ARGS &> /dev/null &
+    echo "Running on Raspberry Pi (Raspberry Pi OS)"
+else
+    echo "Running on an unsupported operating system"
+    # Add your command for unsupported systems here if needed
+fi
